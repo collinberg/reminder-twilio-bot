c200b657fc0dfb56e30f19cc4a0fbde0445f7f15
Collin B <berg.collin@gmail.com>
2019-04-04 20:39:41 -0400

    added twilio api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
@@ -10,6 +11,20 @@ def dope():
 def hello_world():
     return 'Hello World!'
 
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
 
 if __name__ == '__main__':
     app.run()
+    
+
+
+
